1b0e35d88d994923687c716de76055225e50f1cb
IMxSPYDER <hostingpurposee@gmail.com>
2026-05-17 18:08:22 +0530

    final update

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,15 @@ import noteRoutes from "./routes/noteRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://note-app-epifi.netlify.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
